1941d6de5825275403a3cbc82752aa56d2272cc9
Dan Zhuravlov <yuoirty@gmail.com>
2022-12-08 10:44:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/upgradedseries/Item.java
@@ -0,0 +1,17 @@
+package com.example.upgradedseries;
+
+public class Item {
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Item(String value) {
+        this.value = value;
+    }
+}
